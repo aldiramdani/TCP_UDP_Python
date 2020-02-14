1d85988d26d82174e325407ac6f5f2c5ec7dc1df
Aldi Ramdani <aldiramdani8@gmail.com>
2020-02-14 22:19:28 +0700

    ini tugas sister

--- tcp_client.py
+++ tcp_client.py
@@ -4,7 +4,7 @@ TCP_PORT = 8080
 BUFFER_SIZE = 1024
 PESAN = 'Halo Kelompok Kami Beranggota kan 1. Aldi Ramdani , 2. Rivkal Sanjaya, 3. Aulia Ikvanda Yoren'
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET,socket .SOCK_STREAM)
 s.connect((TCP_IP,TCP_PORT))
 s.send(PESAN.encode())
 data = s.recv(BUFFER_SIZE)
